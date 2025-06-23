29b148053421d9681648fdde04229c0b82900a55
Eliran Shemesh <elirans@steps.me>
2025-06-23 14:35:37 +0300

    Fix meta color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import CalendarView from "./CalendarView";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#242424');
-  }, []);
-
   return (
     <section className="w-full flex flex-col items-center justify-center">
       <CalendarView />
