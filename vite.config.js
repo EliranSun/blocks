70d50c6b2ad2d1660beb2e2330ac0e226dc730ee
Eliran Shemesh <piro27@gmail.com>
2025-07-03 17:32:51 +0300

    Add logo?

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,43 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    eslint(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        name: "Bloom",
+        short_name: "Bloom",
+        description: "Bloom",
+        theme_color: "#242424",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 })
