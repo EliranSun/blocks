1f57c08795539a3a54060b760fcb69b373c5ce8b
Eliran Shemesh <piro27@gmail.com>
2025-07-10 12:35:07 +0300

    Update useHabitsByDay.js

--- src/hooks/useHabitsByDay.js
+++ src/hooks/useHabitsByDay.js
@@ -1,10 +1,10 @@
 import { useMemo } from "react";
 import { formatDate, getStorageBySuffix } from "../utils/strorage";
 
-export const useHabitsByDay = (date) => {
+export const useHabitsByDay = (name, date) => {
     const habitsByDay = useMemo(() => {
         const formattedDate = formatDate(date);
-        return getStorageBySuffix(formattedDate);
+        return getStorageBySuffix(name, formattedDate);
     }, [date]);
 
     return habitsByDay;
