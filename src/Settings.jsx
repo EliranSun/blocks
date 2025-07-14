c1cf82fabd7fad198f484e8c1a1319e3fb4732f7
Eliran Shemesh <piro27@gmail.com>
2025-07-14 09:28:31 +0300

    Update Settings.jsx

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -13,11 +13,12 @@ export const Settings = ({ onHomeClick, onHabitsClick, onNotesClick }) => {
     return (
         <div className={classNames(
             "fixed z-20 inset-0 m-auto flex flex-col w-full h-screen",
-            "justify-evenly items-center text-6xl",
-            "merriweather-500 text-center"
+            "justify-evenly items-start text-6xl",
+            "merriweather-500 text-start"
         )}>
             <Title onClick={onHomeClick}>Home</Title>
             <Title onClick={onHabitsClick}>Habits</Title>
+                        <Title onClick={onNotesClick}>Calendar</Title>
             <Title onClick={onNotesClick}>Notes</Title>
             <Title onClick={triggerImport}>Import</Title>
             <Title onClick={exportCalendarData}>Export</Title>
